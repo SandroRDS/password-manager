a04ad12581858773999a7aaee06906cc8d3ffbc7
SandroRDS <sandror.desouza847@gmail.com>
2023-06-10 22:25:55 -0300

    Removendo links inexistentes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AppMobile from './AppMobile.tsx'
-import './index.css'
+import AppMobile from './AppMobile'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
