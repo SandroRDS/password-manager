cf805931d00200973b23aaf9b5788e4944bb0f12
SandroRDS <sandror.desouza847@gmail.com>
2023-07-01 13:36:42 -0300

    Removendo importações inexistentes

--- src/AppMobile.tsx
+++ src/AppMobile.tsx
@@ -1,16 +1,8 @@
 import { Routes, Route } from "react-router-dom";
-import Logon from './pages/Logon';
-import Register from "./pages/Register";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
 
 function AppMobile() {
   return (
     <Routes>
-      <Route path="/" element={ <Logon /> } />
-      <Route path="/register" element={ <Register /> } />
-      <Route path="/home" element={ <Home /> } />
-      <Route path="*" element={ <NotFound /> } />
     </Routes>
   );
 }
