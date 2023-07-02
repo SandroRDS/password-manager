1564d9c97615a387fb58c80d2ac2a55f276fad49
SandroRDS <sandror.desouza847@gmail.com>
2023-07-02 14:05:47 -0300

    Adicionando instância padrão de storageAdapter

--- src/services/storage/index.ts
+++ src/services/storage/index.ts
@@ -1,14 +1,16 @@
-import IStorageAdapter from "../../adapters/storage/IStorageAdapter";
+import IStorageAdapter from '../../adapters/storage/IStorageAdapter';
+import StorageAdapter from '../../adapters/storage/StorageAdapter';
+import StorageKeys from '../../types/StorageKeys';
 
 export default class StorageService {
-  constructor(private storageAdapter: IStorageAdapter) {}
+  constructor(private storageAdapter: IStorageAdapter = new StorageAdapter) {}
 
   saveItem<ItemStorage>(key: string, value: ItemStorage): void {
     return this.storageAdapter.saveItem<ItemStorage>(key, value);
   }
 
   getItem<ItemStorage>(key: string): ItemStorage | null {
-    return this.storageAdapter.getItem<ItemStorage>(key);
+    return this.storageAdapter.getItem<ItemStorage>(key as StorageKeys);
   }
 
   deleteItem(key: string): void {
